1d6f3244df009142b1d7753a3ca4368093dc4aa5
joaobrum108 <joaobrum108gmail.com>
2025-11-26 17:47:39 -0300

    api atualizada

---
+++ src/utils/statusCodes.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  IMAGEM_SALVA: "Imagem salva com sucesso",
+  SUCESSO: "Operação realizada com sucesso",
+  SUCESSO_ATUALIZADO: "Post atualizado com sucesso",
+  SUCESSO_DELETADO: "Post deletado com sucesso",
+
+  NENHUMA_IMAGEM_ENVIADA: "Nenhuma imagem enviada. Envie uma imagem válida.",
+
+  POST_NAO_ENCONTRADO: "Post não encontrado",
+
+  ERRO_INSERIR_DADOS: "Erro ao inserir dados no banco",
+  ERRO_BUSCA_DADOS: "Erro ao buscar registros",
+  ERRO_BUSCA_POR_ID: "Erro ao buscar registro por ID",
+  ERRO_ATUALIZACAO: "Erro ao atualizar registro",
+  ERRO_DELETAR_REGISTRO: "Erro ao deletar registro"
+};
